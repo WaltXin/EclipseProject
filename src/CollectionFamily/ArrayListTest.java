1c19ea51777dd8e4d3928386f544963990bac65f
WaltXin <xiongx@tcd.ie>
2016-07-13 20:24:23 +0100

    Add Immutable String and mutable StringBuilder

--- src/CollectionFamily/ArrayListTest.java
+++ src/CollectionFamily/ArrayListTest.java
@@ -35,6 +35,24 @@ public class ArrayListTest {
 			System.out.println(l.get(i));
 			i++;
 		}
+		
+		//add immutable String
+		for(String string : l)
+		{
+			string += "1";
+		}
+		System.out.println(l);
+		
+		//add mutable StringBuilder
+		ArrayList<StringBuilder> als = new ArrayList<StringBuilder>();
+		StringBuilder sBuilder = new StringBuilder();
+		als.add(new StringBuilder("a"));
+		als.add(new StringBuilder("b"));
+		for (StringBuilder sb : als)
+		{
+			sb.append("1");
+		}
+		System.out.println(als);
 	}
 
 }
